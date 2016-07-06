fa56cd29dceb88d8deee0f71c3b8ab06c2d41beb
palmtree5 <remlap5.ap@gmail.com>
2016-07-05 20:10:13 -0500

    Possible fix for issue

--- hpapi/hpapi.py
+++ hpapi/hpapi.py
@@ -24,9 +24,10 @@ class hpapi():
             await send_cmd_help(ctx)
 
     @_hpapi.command(pass_context=True, no_pm=True, name='booster')
-    async def _booster(self, ctx, game=None):
+    async def _booster(self, ctx):
         """Get active boosters. A game can be specified, in which case only the active booster for that game and the number of queued boosters for that game will be shown"""
         payload = self.payload
+        game = None
         url = 'http://api.hypixel.net/boosters?key=' + payload["apikey"]
         conn = aiohttp.TCPConnector(verify_ssl=False)
         sess = aiohttp.ClientSession(connector=conn)
