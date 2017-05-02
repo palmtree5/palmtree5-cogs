d81c95f415d1f9f70833ece2cc43bfc11fc083a5
palmtree5 <palmtree5@users.noreply.github.com>
2017-05-01 19:08:41 -0800

    [Catfact] initial v3 updates

--- catfact/catfact.py
+++ catfact/catfact.py
@@ -6,17 +6,16 @@ class Catfact():
     """A cog for getting a random cat fact"""
     def __init__(self, bot):
         self.bot = bot
+        self.session = aiohttp.ClientSession()
 
-    @commands.command(no_pm=True, pass_context=True, name="catfact")
+    @commands.command(pass_context=True, name="catfact")
     async def _catfact(self, ctx):
         """Gets a random cat fact"""
-        async with aiohttp.get("http://catfacts-api.appspot.com/api/facts") as cfget:
+        async with self.session.get("http://catfacts-api.appspot.com/api/facts") as cfget:
             fact_json = await cfget.json()
         fact = fact_json["facts"][0]
-        await self.bot.say("Ok " + ctx.message.author.mention + ", here is a cat fact.\n" + fact)
-
+        await ctx.send("Ok " + ctx.message.author.mention + ", here is a cat fact.\n" + fact)
 
 
 def setup(bot):
-    n = Catfact(bot)
-    bot.add_cog(n)
+    bot.add_cog(Catfact(bot))
