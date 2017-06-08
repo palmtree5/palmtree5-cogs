1cb457d809b4b3e91a7c2510641fa67ba4c5a095
palmtree5 <palmtree5@users.noreply.github.com>
2017-06-08 12:26:03 -0800

    [MentionTracker] Implement Cog-Creators/Cog-Board/issues/51

--- mentiontracker/mentiontracker.py
+++ mentiontracker/mentiontracker.py
@@ -1,7 +1,12 @@
 from discord.ext import commands
 import discord
 import os
+import logging
 from .utils.dataIO import dataIO
+from .utils.chat_formatting import box
+
+
+log = logging.getLogger("red.mentiontracker")
 
 
 class MentionTracker():
@@ -28,19 +33,165 @@ class MentionTracker():
         else:
             await self.bot.say("Invalid status specified!")
 
+    @commands.command(pass_context=True)
+    async def trackword(self, ctx: commands.Context, word: str, channel: discord.Channel=None):
+        """Toggle tracking of the specified word in the specified channel
+        Required parameters:
+        - word: the word to add or remove
+          - if the word is in the list, it will be removed
+          - if it is not in the list, it will be added
+        Optional parameters:
+        - channel: the channel to track the word for (defaults to the current channel if not specified)"""
+        author = ctx.message.author
+        if not channel:
+            channel = ctx.message.channel
+        if author.id not in self.settings:
+            self.settings[author.id] = {}
+        if "words" not in self.settings[author.id]:
+            self.settings[author.id]["words"] = {}
+        if channel.id not in self.settings[author.id]["words"]:
+            self.settings[author.id]["words"][channel.id] = []
+        added = False
+        cur_list = self.settings[author.id]["words"][channel.id]
+        if word not in cur_list:
+            cur_list.append(word)
+            added = True
+        else:
+            cur_list.remove(word)
+        self.settings[author.id]["words"][channel.id] = cur_list
+        dataIO.save_json("data/mentiontracker/settings.json", self.settings)
+        if added:
+            await self.bot.say("Added '{}' to the tracking list for {}".format(word, channel.mention))
+        else:
+            await self.bot.say("Removed '{}' from the trackling list for {}".format(word, channel.mention))
+
+    @commands.command(pass_context=True)
+    async def listwords(self, ctx: commands.Context, channel: discord.Channel=None):
+        """List all words for the specified channel
+        Optional parameters:
+        - channel: the channel to show words for (defaults to the current channel)"""
+        author = ctx.message.author
+        if not channel:
+            channel = ctx.message.channel
+        if author.id not in self.settings or\
+                "words" not in self.settings[author.id] or\
+                channel.id not in self.settings[author.id]["words"] or\
+                not self.settings[author.id]["words"][channel.id]:
+            await self.bot.say("You haven't set any words to be tracked!")
+        else:
+            head = "Tracked words for {}#{} in #{}".format(author.name, author.discriminator, channel.name)
+            msg = ""
+            for word in self.settings[author.id]["words"][channel.id]:
+                msg += "{}\n".format(word)
+            await self.bot.say(box(msg, lang=head))
+
     async def check_message(self, message):
+        """Check function for checking user mention settings
+        and the user's word list for the message's channel"""
         if len(message.mentions) > 0:
             for mention in message.mentions:
                 if mention.id in self.settings and message.channel.id in\
                         self.settings[mention.id] and\
                         self.settings[mention.id][message.channel.id]:
-                    emb = discord.Embed(title="You were mentioned!", description=message.content)
-                    emb.add_field(name="Mentioner", value=message.author.name + "#" + message.author.discriminator, inline=False)
-                    emb.add_field(name="Server", value=message.server.name, inline=False)
-                    emb.add_field(name="Channel", value=message.channel.name, inline=False)
+                    emb = discord.Embed(
+                        title="You were mentioned!",
+                        description=message.content
+                    )
+                    emb.add_field(
+                        name="Mentioner",
+                        value="{}#{}".format(
+                            message.author.name,
+                            message.author.discriminator
+                        ),
+                        inline=False
+                    )
+                    emb.add_field(
+                        name="Server",
+                        value=message.server.name,
+                        inline=False
+                    )
+                    emb.add_field(
+                        name="Channel",
+                        value=message.channel.name,
+                        inline=False
+                    )
                     emb.set_footer(text=str(message.timestamp) + " UTC")
-                    user = discord.utils.get(self.bot.get_all_members(), id=mention.id)
-                    await self.bot.send_message(user, embed=emb)
+                    user = [m for m in message.server.members if m.id == mention.id][0]
+                    try:
+                        await self.bot.send_message(user, embed=emb)
+                    except discord.Forbidden:
+                        log.warning(
+                            "Attempted to send a message to user with id {} "
+                            "but failed due to permissions!".format(user.id)
+                        )
+                    except discord.NotFound:
+                        log.warning(
+                            "Destination not found!"
+                        )
+                    except discord.InvalidArgument:
+                        log.warning(
+                            "Invalid destination!"
+                        )
+                    except discord.HTTPException:
+                        log.warning(
+                            "Something went wrong!"
+                        )
+                    else:
+                        return
+        if message.content:
+            for user in list(self.settings.keys()):
+                if "words" in self.settings[user] and\
+                        message.channel.id in self.settings[user]["words"]\
+                        and self.settings[user]["words"][message.channel.id]:
+                    for word in self.settings[user]["words"][message.channel.id]:
+                        if word in message.content and user != message.author.id:
+                            emb = discord.Embed(
+                                title="A word you're subscribed to was used!",
+                                description=message.content
+                            )
+                            emb.add_field(
+                                name="Message author",
+                                value="{}#{}".format(
+                                    message.author.name,
+                                    message.author.discriminator
+                                ),
+                                inline=False
+                            )
+                            emb.add_field(
+                                name="Server",
+                                value=message.server.name,
+                                inline=False
+                            )
+                            emb.add_field(
+                                name="Channel",
+                                value=message.channel.name,
+                                inline=False
+                            )
+                            emb.set_footer(
+                                text=str(message.timestamp) + " UTC"
+                            )
+                            user = [m for m in message.server.members if m.id == user][0]
+                            try:
+                                await self.bot.send_message(user, embed=emb)
+                            except discord.Forbidden:
+                                log.warning(
+                                    "Attempted to send a message to user with id {} "
+                                    "but failed due to permissions!".format(user.id)
+                                )
+                            except discord.NotFound:
+                                log.warning(
+                                    "Destination not found!"
+                                )
+                            except discord.InvalidArgument:
+                                log.warning(
+                                    "Invalid destination!"
+                                )
+                            except discord.HTTPException:
+                                log.warning(
+                                    "Something went wrong!"
+                                )
+                            else:
+                                return
 
 
 def check_folder():
