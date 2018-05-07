327a0112debab6806e87f39ba3038a76b1f392df
palmtree5 <3577255+palmtree5@users.noreply.github.com>
2018-05-07 15:17:47 -0800

    Python version check in init

--- hpapi/__init__.py
+++ hpapi/__init__.py
@@ -1,8 +1,12 @@
-from .hpapi import Hpapi
+import sys
+
 from redbot.core import data_manager
 
 
 def setup(bot):
+    if sys.version_info < (3, 6, 0):
+        raise RuntimeError("This cog requires Python 3.6")
+    from .hpapi import Hpapi
     n = Hpapi(bot)
     data_manager.load_bundled_data(n, __file__)
     bot.add_cog(n)
