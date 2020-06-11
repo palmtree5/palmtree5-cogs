1b20d33c6505fd47dd2db3a3511d97c15785748f
palmtree5 <3577255+palmtree5@users.noreply.github.com>
2020-06-10 20:00:38 -0800

    Attempt adding a link to the comments + adding thumbnail

--- reddit/helpers.py
+++ reddit/helpers.py
@@ -59,7 +59,9 @@ def post_embed(data: dict, now: dt) -> discord.Embed:
         em.add_field(name="Stickied", value="Yes")
     else:
         em.add_field(name="Stickied", value="No")
-    em.add_field(name="Comments", value=str(data["data"]["num_comments"]))
+    em.add_field(name="Comments", value="[{}]({})".format(data["data"]["num_comments"], "https://reddit.com{}".format(data["data"]["permalink"]))
+    if data["data"]["thumbnail"] != "self":
+        em.set_thumbnail(url=data["data"]["thumbnail"])
     return em
 
 
