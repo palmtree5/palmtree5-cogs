6c9a7eb5824bc98cd34736465eb02dfbadb9a88e
palmtree5 <3577255+palmtree5@users.noreply.github.com>
2020-09-21 21:02:08 -0800

    Missed a bit that is needed to make the statements show

--- banrole/__init__.py
+++ banrole/__init__.py
@@ -1,5 +1,11 @@
+import json
+from pathlib import Path
 from .banrole import BanRole
 import asyncio
+
+
+with open(Path(__file__).parent / "info.json") as infofile:  # Happen to like Jack's way of constructing this from what's in the info.json file so this is very much courtesy of him
+    __red_end_user_data_statement__ = json.load(infofile)["end_user_data_statement"]
 
 
 async def setup(bot):
--- hpapi/__init__.py
+++ hpapi/__init__.py
@@ -1,7 +1,11 @@
+from pathlib import Path
 import sys
 from .hpapi import Hpapi
 import asyncio
+import json
 
+with open(Path(__file__).parent / "info.json") as infofile:  # Happen to like Jack's way of constructing this from what's in the info.json file so this is very much courtesy of him
+    __red_end_user_data_statement__ = json.load(infofile)["end_user_data_statement"]
 
 async def setup(bot):
     if sys.version_info < (3, 6, 0):
